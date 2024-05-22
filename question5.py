e926fc3a03420f4d0b86a31c77d21e941bb9ce0f
MitraSenthilkumar <153517551+MitraSenthilkumar@users.noreply.github.com>
2024-05-22 10:35:26 +0530

    Update question5.py

--- question5.py
+++ question5.py
@@ -1,1 +1,11 @@
 #write a program to find if the given number is prime no or not
+n = int(input("Enter a number: "))
+if n > 1:
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            print("Not a prime number")
+            break
+    else:
+        print("Prime number")
+else:
+    print("Not a prime number")
