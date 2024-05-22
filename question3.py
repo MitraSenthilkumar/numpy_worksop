3cfaa022b6a8c2f4bf499a806a63d4f4a5974cf9
MitraSenthilkumar <153517551+MitraSenthilkumar@users.noreply.github.com>
2024-05-22 10:17:20 +0530

    Update question3.py

--- question3.py
+++ question3.py
@@ -1,1 +1,12 @@
 #write a program to find the factorial of a nummber
+n = int(input("Enter a number: "))
+factorial = 1
+
+if n < 0:
+    print("No negative numbers")
+elif n == 0:
+    print("The factorial of 0 is 1")
+else:
+    for i in range(1, n + 1):
+        factorial *= i
+    print("The factorial of is", factorial)
