e04cef82b94ece1534949c837fb09be15a593d55
MitraSenthilkumar <153517551+MitraSenthilkumar@users.noreply.github.com>
2024-05-22 10:33:08 +0530

    Update question4.py

--- question4.py
+++ question4.py
@@ -1,1 +1,8 @@
 #write a program to find the sum of digits of a given number'
+num = int(input("Enter a number: "))
+sum = 0
+
+for i in range(1, num + 1):
+    sum += i
+
+print("The sum of numbers from  is", sum)
