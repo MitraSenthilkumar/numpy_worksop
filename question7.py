a9acc0974df455399883b92a284400073baf34ac
MitraSenthilkumar <153517551+MitraSenthilkumar@users.noreply.github.com>
2024-05-22 10:49:24 +0530

    Update question7.py

--- question7.py
+++ question7.py
@@ -10,3 +10,15 @@ e=[3]
 
 # use append method to do this.
 
+l1 = []
+a=[1,2,3]
+b=[4,5,6]
+c=[7,5,3,2,3]
+d=[2]
+e=[3]
+l1.append(a)
+l1.append(b)
+c.append(d)
+d.append(e)
+l1.append(c)
+print(l1)
